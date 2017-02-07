624f786c43fad5a73a8f1f8bfa4f4505d0a897c6
James <James@172-22-74-142.epm.br>
2017-02-07 14:29:11 -0200

    Add demo version of instalar_pacotes

---
+++ instalar_pacotes_demo.R
@@ -0,0 +1,9 @@
+## Programa para automatizar o download dos pacotes - Versão para Demonstração
+## necessários para iniciar MAD-CB
+## Author: James Hunter
+## Date: 7/2/17
+## Version: 1.0a
+
+pacotes2 <- c("forecast", "foreign")
+
+install.packages(pacotes2)
